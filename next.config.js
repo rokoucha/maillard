136a0f18d98716e6d7ab12b3fc830d0d65c85e87
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2024-11-08 00:16:18 +0900

    static export

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+export default {
+  output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+}
