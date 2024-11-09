100b80f4d4f1d7fc2b34d4d55757cba1b92bf2b8
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2024-11-09 22:31:18 +0900

    fix keisihiki

--- public/functions/api/_build.js
+++ public/functions/api/_build.js
@@ -2,24 +2,22 @@ const TIMER_KEY = 'called'
 const TIMEOUT_MS = 1000 * 60 * 5 // 5 minutes
 const WAIT_MS = 60000 // 1 minute
 
-export default {
-  async fetch(request, env) {
-    const now = Date.now()
+export async function onRequest(context) {
+  const now = Date.now()
 
-    let value = await env.KV.get(TIMER_KEY)
+  let value = await context.env.KV.get(TIMER_KEY)
 
-    if (value === null) {
-      await env.KV.put(TIMER_KEY, now)
-      value = now
-    }
+  if (value === null) {
+    await context.env.KV.put(TIMER_KEY, now)
+    value = now
+  }
 
-    while (now - value < TIMEOUT_MS) {
-      value = await env.KV.get(TIMER_KEY)
-      await new Promise((resolve) => setTimeout(resolve, WAIT_MS))
-    }
+  while (now - value < TIMEOUT_MS) {
+    value = await env.KV.get(TIMER_KEY)
+    await new Promise((resolve) => setTimeout(resolve, WAIT_MS))
+  }
 
-    await env.KV.put(TIMER_KEY, now)
+  await context.env.KV.put(TIMER_KEY, now)
 
-    return await fetch(env.WEBHOOK_URL, { method: 'post' })
-  },
+  return await fetch(context.env.WEBHOOK_URL, { method: 'post' })
 }
