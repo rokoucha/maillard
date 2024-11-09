8c6fb4e60c7f819bb0af322f813e427df43eb456
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2024-11-09 23:58:34 +0900

    exclusive

--- functions/api/_build.js
+++ functions/api/_build.js
@@ -1,9 +1,11 @@
 const TIMER_KEY = 'called'
+const EXCLUSIVE_KEY = 'exclusive'
 const TIMEOUT_MS = 1000 * 60 * 5 // 5 minutes
 const WAIT_MS = 60000 // 1 minute
 
 export async function onRequest(context) {
-  let now = Date.now()
+  const now = Date.now()
+  const exclusiveId = context.request.headers.get('cf-ray')
 
   let value = await context.env.KV.get(TIMER_KEY)
 
@@ -12,9 +14,15 @@ export async function onRequest(context) {
     value = now
   }
 
-  while (now - value < TIMEOUT_MS) {
-    now = Date.now()
+  await context.env.KV.put(EXCLUSIVE_KEY, exclusiveId)
+
+  while (Date.now() - value < TIMEOUT_MS) {
     await new Promise((resolve) => setTimeout(resolve, WAIT_MS))
+
+    const e = await context.env.KV.get(EXCLUSIVE_KEY)
+    if (e !== exclusiveId) {
+      return new Response('Exclusive lock lost', { status: 200 })
+    }
   }
 
   await context.env.KV.delete(TIMER_KEY)
