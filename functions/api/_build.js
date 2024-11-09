f69cfdc96316eb1220aaa94f96c76bdba670d7a9
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2024-11-09 22:19:52 +0900

    webhook遅延くん

---
+++ functions/api/_build.js
@@ -0,0 +1,25 @@
+const TIMER_KEY = 'called'
+const TIMEOUT_MS = 1000 * 60 * 5 // 5 minutes
+const WAIT_MS = 60000 // 1 minute
+
+export default {
+  async fetch(request, env) {
+    const now = Date.now()
+
+    let value = await env.KV.get(TIMER_KEY)
+
+    if (value === null) {
+      await env.KV.put(TIMER_KEY, now)
+      value = now
+    }
+
+    while (now - value < TIMEOUT_MS) {
+      value = await env.KV.get(TIMER_KEY)
+      await new Promise((resolve) => setTimeout(resolve, WAIT_MS))
+    }
+
+    await env.KV.put(TIMER_KEY, now)
+
+    return await fetch(env.WEBHOOK_URL, { method: 'post' })
+  },
+}
