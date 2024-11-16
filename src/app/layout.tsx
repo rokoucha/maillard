238ca5ebd771b3c2469f4d2f99a593a11136d8f5
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2024-11-16 17:51:58 +0900

    FontAwesomeプリロードしてみる

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,11 +61,12 @@ export default function RootLayout({
     <html lang={SITE_LANG}>
       <head>
         <link
-          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
           integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-          crossOrigin="anonymous"
           referrerPolicy="no-referrer"
+          rel="preload"
         />
       </head>
       <body>{children}</body>
