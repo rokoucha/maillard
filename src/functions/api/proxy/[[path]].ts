a3c364ab5e57e74d3cba3613f6211916fcba50bc
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2025-01-01 19:50:38 +0900

    メモリにキャッシュを押し込んでみる

--- src/functions/api/proxy/[[path]].ts
+++ src/functions/api/proxy/[[path]].ts
@@ -1,6 +1,4 @@
 interface Env {
-  KV: KVNamespace
-  SCRAPBOX_PROXY_SALT: string
   SCRAPBOX_PROXY_TTL: string
 }
 
@@ -20,18 +18,6 @@ function sanitzeResponseHeaders(headers: Headers) {
     [...headers.entries()].filter(([name]) =>
       ALLOWED_RESPONSE_HEADERS.includes(name.toLowerCase()),
     ),
-  )
-}
-
-function base64Encode(bytes: Uint8Array) {
-  return btoa(bytes.reduce((s, b) => s + String.fromCharCode(b), ''))
-}
-
-function base64Decode(str: string) {
-  return new Uint8Array(
-    atob(str)
-      .split('')
-      .map((c) => c.charCodeAt(0)),
   )
 }
 
@@ -56,110 +42,20 @@ async function digestRequest(request: Request) {
     .join('')
 }
 
-async function deriveKey(passphrase: string, salt: string) {
-  const iterations = 100000
-  const key = await crypto.subtle.importKey(
-    'raw',
-    new TextEncoder().encode(passphrase),
-    {
-      name: 'PBKDF2',
-    },
-    false,
-    ['deriveKey'],
-  )
-
-  return crypto.subtle.deriveKey(
-    {
-      name: 'PBKDF2',
-      salt: new Uint8Array(new TextEncoder().encode(salt)).buffer,
-      iterations,
-      hash: 'SHA-256',
-    },
-    key,
-    {
-      name: 'AES-GCM',
-      length: 256,
-    },
-    false,
-    ['encrypt', 'decrypt'],
-  )
+type CacheEntry = {
+  body: ReadableStream
+  created: number
+  headers: Headers
+  status: number
 }
 
-async function encryptResponse(key: CryptoKey, response: Response) {
-  const metadata = new TextEncoder().encode(
-    JSON.stringify({
-      status: response.status,
-      headers: Object.fromEntries(sanitzeResponseHeaders(response.headers)),
-    }),
-  )
-
-  const iv = crypto.getRandomValues(new Uint8Array(12))
-
-  const metadataEncrypted = await crypto.subtle.encrypt(
-    {
-      name: 'AES-GCM',
-      iv: iv.buffer,
-    },
-    key,
-    metadata,
-  )
-
-  const body = await response.arrayBuffer()
-
-  const bodyEncrypted = await crypto.subtle.encrypt(
-    {
-      name: 'AES-GCM',
-      iv: iv.buffer,
-    },
-    key,
-    body,
-  )
-
-  return {
-    metadata: [
-      base64Encode(iv),
-      base64Encode(new Uint8Array(metadataEncrypted)),
-    ].join(':'),
-    body: bodyEncrypted,
-  }
-}
-
-async function decryptResponse(
-  key: CryptoKey,
-  metadata: string,
-  body: ArrayBuffer,
-) {
-  const [ivBase64, metadataBase64] = metadata.split(':')
-
-  const iv = base64Decode(ivBase64)
-
-  const metadataDecrypted = await crypto.subtle.decrypt(
-    {
-      name: 'AES-GCM',
-      iv: iv.buffer,
-    },
-    key,
-    base64Decode(metadataBase64).buffer,
-  )
-
-  const metadataObject = JSON.parse(new TextDecoder().decode(metadataDecrypted))
-
-  const bodyDecrypted = await crypto.subtle.decrypt(
-    {
-      name: 'AES-GCM',
-      iv: iv.buffer,
-    },
-    key,
-    body,
-  )
-
-  return {
-    metadata: metadataObject,
-    body: bodyDecrypted,
-  }
-}
+const cache = new Map<string, CacheEntry>()
 
 export const onRequest: PagesFunction<Env> = async (context) => {
+  const now = Date.now()
+
+  const proxyTtl = Number(context.env.SCRAPBOX_PROXY_TTL)
+
   const path = Array.isArray(context.params.path)
     ? context.params.path.join('/')
     : context.params.path
@@ -176,50 +72,22 @@ export const onRequest: PagesFunction<Env> = async (context) => {
     ),
   })
 
-  let key: CryptoKey | null = null
-  const cookie = context.request.headers.get('cookie')
-  if (cookie) {
-    key = await deriveKey(cookie, context.env.SCRAPBOX_PROXY_SALT)
-  }
+  const key = await digestRequest(request)
 
-  const cacheKey = await digestRequest(request)
-
-  const { value, metadata } = await context.env.KV.getWithMetadata<string>(
-    cacheKey,
-    'arrayBuffer',
-  )
-
-  if (value && metadata) {
-    if (!key) {
-      const metadataObject = JSON.parse(metadata)
-      return new Response(value, {
-        status: metadataObject.status,
-        headers: {
-          ...metadataObject.headers,
-          'X-Cache': 'HIT',
-          'X-Cache-Encrypted': 'NO',
-        },
-      })
-    }
-
-    const { metadata: metadataObject, body } = await decryptResponse(
-      key,
-      metadata,
-      value,
-    )
-    return new Response(body, {
-      status: metadataObject.status,
+  const cached = cache.get(key)
+  if (cached && now - cached.created < proxyTtl) {
+    return new Response(cached.body, {
+      status: cached.status,
       headers: {
-        ...metadataObject.headers,
+        ...cached.headers.entries(),
         'X-Cache': 'HIT',
-        'X-Cache-Encrypted': 'YES',
       },
     })
   }
 
   const response = await fetch(request)
 
-  if (!response.ok) {
+  if (!response.ok || !response.body) {
     return new Response(response.body, {
       status: response.status,
       headers: {
@@ -229,38 +97,11 @@ export const onRequest: PagesFunction<Env> = async (context) => {
     })
   }
 
-  const expirationTtl = Number(context.env.SCRAPBOX_PROXY_TTL)
-
-  if (!key) {
-    const metadata = JSON.stringify({
-      status: response.status,
-      headers: Object.fromEntries(sanitzeResponseHeaders(response.headers)),
-    })
-
-    // cache without encryption
-    await context.env.KV.put(cacheKey, await response.clone().arrayBuffer(), {
-      expirationTtl,
-      metadata,
-    })
-
-    return new Response(response.body, {
-      status: response.status,
-      headers: {
-        ...Object.fromEntries(sanitzeResponseHeaders(response.headers)),
-        'X-Cache': 'MISS',
-        'X-Cache-Encrypted': 'NO',
-      },
-    })
-  }
-
-  const { metadata: encryptedMetadata, body } = await encryptResponse(
-    key,
-    response.clone(),
-  )
-
-  await context.env.KV.put(cacheKey, body, {
-    expirationTtl,
-    metadata: encryptedMetadata,
+  cache.set(key, {
+    created: now,
+    status: response.status,
+    headers: response.headers,
+    body: response.body,
   })
 
   return new Response(response.body, {
@@ -268,7 +109,6 @@ export const onRequest: PagesFunction<Env> = async (context) => {
     headers: {
       ...Object.fromEntries(sanitzeResponseHeaders(response.headers)),
       'X-Cache': 'MISS',
-      'X-Cache-Encrypted': 'YES',
     },
   })
 }
