b473390a6195ab778d97e69b59639e95cb76ad61
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2025-08-17 16:10:49 +0900

    さようならPageInfo

--- src/lib/actions/cosense.ts
+++ src/lib/actions/cosense.ts
@@ -1,14 +1,6 @@
 import { cache } from 'react'
 import { PageResponse } from '../presentation/page'
-import { type PageInfoResponse } from '../presentation/pageinfo'
 import * as PageUsecase from '../usecase/page'
-import * as PageInfoUsecase from '../usecase/pageinfo'
-
-const getPageList = cache(async function getPageList(): Promise<
-  PageInfoResponse[]
-> {
-  return await PageInfoUsecase.findMany()
-})
 
 export const getPage = cache(async function getPage(
   title: string,
